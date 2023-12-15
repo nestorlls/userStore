71d5662343395ec7c64d589d0d6dabc9b2a30604
Paul Llanque <llanquepol@gmail.com>
2023-12-15 18:03:03 -0500

    [UPDATE] mongo connection added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs';
+import { MongoConnection } from './data';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
@@ -6,10 +7,18 @@ import { Server } from './presentation/server';
   main();
 })();
 
-function main() {
+async function main() {
+  const mongoConneted = await MongoConnection.connection({
+    mongoUrl: envs.MONGO_DB_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
   });
-  server.start();
+
+  if (mongoConneted) {
+    console.log('MongoDB connected');
+    server.start();
+  }
 }
