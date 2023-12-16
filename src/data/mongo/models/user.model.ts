d2f35ce4be3e30a4e339b546775f101499aee410
Paul Llanque <llanquepol@gmail.com>
2023-12-15 22:09:12 -0500

    [UPDATE] validate email added

--- src/data/mongo/models/user.model.ts
+++ src/data/mongo/models/user.model.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: { type: String, require: [true, 'Name is required'] },
   email: { type: String, require: [true, 'Email is required'], unique: true },
+  emailValidated: { type: Boolean, default: false },
   password: { type: String, require: [true, 'Password is required'] },
   avatar: { type: String },
   role: { type: [String], enum: ['user', 'admin'], default: ['user'] },
