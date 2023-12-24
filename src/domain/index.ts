d804a2d75c1470db3298c9d7ed982f90492deffa
Paul Llanque <llanquepol@gmail.com>
2023-12-23 20:35:34 -0500

    [UPDATE] export domains

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,13 +1,22 @@
 export * from './errors/custome.error';
+
 export * from './entities/useEntity';
 export * from './entities/categoryEntity';
+export * from './entities/ProductEntity';
+
 export * from './dtos/auth/register-user.dto';
 export * from './dtos/auth/login-user.dto';
 export * from './dtos/category/create-category.dto';
 export * from './dtos/category/update-category.dto';
+export * from './dtos/product/create-product.dto';
+export * from './dtos/product/update-product.dto';
 export * from './dtos/shared/pagination.dto';
-export * from './dtos/category/pagination-return.dto';
+export * from './dtos/shared/pagination-return.dto';
+
 export * from './datasources/category.datasource';
 export * from './datasources/auth.datasource';
+export * from './datasources/product.datasource';
+
 export * from './repository/category.repository';
 export * from './repository/auth.repository';
+export * from './repository/product.repository';
