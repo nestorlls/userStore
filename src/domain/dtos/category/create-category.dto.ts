817b3e03f40e03f86329f6fbac70f045492bba1c
Paul Llanque <llanquepol@gmail.com>
2023-12-23 20:30:36 -0500

    [REFACTOR] create category dto

--- src/domain/dtos/category/create-category.dto.ts
+++ src/domain/dtos/category/create-category.dto.ts
@@ -1,16 +1,21 @@
+import { Validators } from '../../../config';
+
 export class CreateCategoryDto {
   private constructor(
     public readonly name: string,
-    public readonly available: boolean
+    public readonly available: boolean,
+    public readonly user: string
   ) {}
 
-  static create(obj: { [key: string]: any }): [string?, CreateCategoryDto?] {
-    const { name, available = false } = obj;
+  static create(props: { [key: string]: any }): [string?, CreateCategoryDto?] {
+    const { name, available = false, user } = props;
     let availableBoolean = available;
 
     if (!name) return ['Missing name'];
     if (typeof available !== 'boolean') availableBoolean = available === 'true';
+    if (!user) return ['Missing user'];
+    if (!Validators.isMongoId(user)) return ['Invalid user id'];
 
-    return [undefined, new CreateCategoryDto(name, availableBoolean)];
+    return [undefined, new CreateCategoryDto(name, availableBoolean, user)];
   }
 }
