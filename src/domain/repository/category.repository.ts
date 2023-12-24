37dd763c6810b4defea99e2f753cfaea9f4cc485
Paul Llanque <llanquepol@gmail.com>
2023-12-23 20:27:51 -0500

    [REFACTOR] category datasource and repositore refactored

--- src/domain/repository/category.repository.ts
+++ src/domain/repository/category.repository.ts
@@ -1,17 +1,15 @@
 import { CreateCategoryDto } from '../dtos/category/create-category.dto';
-import { PaginationReturnDto } from '../dtos/category/pagination-return.dto';
+import { PaginationReturnDto } from '../dtos/shared/pagination-return.dto';
 import { UpdateCategoryDto } from '../dtos/category/update-category.dto';
 import { PaginationDto } from '../dtos/shared/pagination.dto';
 import { CategoryEntity } from '../entities/categoryEntity';
 import { UserEntity } from '../entities/useEntity';
 
 export abstract class CategoryRepository {
-  abstract getAll(dto: PaginationDto): Promise<PaginationReturnDto>;
+  /* prettier-ignore */
+  abstract getAll(dto: PaginationDto): Promise<PaginationReturnDto<CategoryEntity>>;
   abstract getbyId(id: string): Promise<CategoryEntity>;
-  abstract create(
-    dto: CreateCategoryDto,
-    user: UserEntity
-  ): Promise<CategoryEntity>;
+  abstract create(dto: CreateCategoryDto): Promise<CategoryEntity>;
   abstract update(dto: UpdateCategoryDto): Promise<CategoryEntity>;
   abstract delete(id: string): Promise<void>;
 }
