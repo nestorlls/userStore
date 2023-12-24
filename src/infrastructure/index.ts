ed42934e474ddaebb9c33d75f5e7a1389e27f2cc
Paul Llanque <llanquepol@gmail.com>
2023-12-23 20:45:01 -0500

    [ADD} exports infrastucture

--- src/infrastructure/index.ts
+++ src/infrastructure/index.ts
@@ -1,4 +1,8 @@
 export * from './datasources/category.datasource.impl';
 export * from './datasources/auth.datasource.impl';
+
 export * from './repository/category.repository.impl';
 export * from './repository/auth.repository.impl';
+
+export * from './datasources/product.datasource.impl';
+export * from './repository/product.repository.impl';
