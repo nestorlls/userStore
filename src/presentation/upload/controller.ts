cc2b06cb2fd4655535bf454b1e4ffc194881f130
Paul Llanque <llanquepol@gmail.com>
2023-12-26 10:10:03 -0500

    [ADD] uploadMultipleFiles added

--- src/presentation/upload/controller.ts
+++ src/presentation/upload/controller.ts
@@ -6,18 +6,25 @@ export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   uploadFile = (req: Request, res: Response) => {
-    const files = req.body.files.at(0) as UploadFile;
-    const type = req.params.type;
+    const file = req.body.files.at(0) as UploadFile;
+    const folderType = req.params.type;
 
     this.uploadService
-      .uploadSingle(files, `uploads/${type}`)
+      .uploadSingle(file, `uploads/${folderType}`)
       .then((upload) => res.status(200).json(upload))
       .catch((error) => this.handleError(error, res))
       .finally(() => console.log('Finally'));
   };
 
   uploadMultipleFiles = (req: Request, res: Response) => {
-    res.status(200).json('Files uploaded successfully');
+    const files = req.body.files;
+    const folderType = req.params.type;
+
+    this.uploadService
+      .uploadMultiple(files, `uploads/${folderType}`)
+      .then((files) => res.status(200).json(files))
+      .catch((error) => this.handleError(error, res))
+      .finally(() => console.log('Finally'));
   };
 
   private handleError(error: unknown, res: Response) {
